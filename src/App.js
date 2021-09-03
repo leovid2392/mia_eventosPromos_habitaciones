ef2158c9d855af62020fc63cf1105ee6ddc52640
Leotori <leovid2392@gmail.com>
2021-09-03 02:19:06 -0500

    ready to deploy true

--- src/App.js
+++ src/App.js
@@ -1,27 +1,13 @@
 import React, { useEffect, useState } from "react";
 
 import Home from "./components/home/Home";
-import MenuList from "./components/menuList/MenuList";
 
-import ReactGa from "react-ga";
 
 import "./App.css";
 
 function App() {
-	const [showHome, setShowHome] = useState(true);
+	return <Home  />;
 
-	useEffect(() => {
-		ReactGa.initialize("G-PY38QBC11Q");
-
-		// to report page view
-		ReactGa.pageview("/");
-	}, []);
-
-	if (showHome) {
-		return <Home showHome={showHome} setShowHome={setShowHome} />;
-	} else {
-		return <MenuList showHome={showHome} setShowHome={setShowHome} />;
-	}
 }
 
 export default App;
