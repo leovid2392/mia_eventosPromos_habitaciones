ca124d7075c5ace88c1d96478509caf170edca91
Leotori <leovid2392@gmail.com>
2021-09-03 20:24:08 -0500

    september 08 event added

--- src/data.js
+++ src/data.js
@@ -1,5 +1,6 @@
 import React from "react";
 import september_05 from "./assets/slider/september_05.jpeg";
+import september_08 from "./assets/slider/september_08.jpeg";
 import september_10 from "./assets/slider/september_10.jpeg";
 import september_12 from "./assets/slider/september_12.jpeg";
 import september_16 from "./assets/slider/september_16.jpeg";
@@ -15,27 +16,34 @@ const sliderData = [
 	},
 	{
 		id: 2,
+		url: "https://www.eventbrite.com.mx/e/mystical-sunset-tickets-169651102013",
+		title: "august 28, especial dinner with francesca lombardo",
+		date: "September 08th | 2 - 11 PM",
+		image: september_08,
+	},
+	{
+		id: 3,
 		url: "https://www.ticketfairy.com/event/matthias-meyer-11sep2021/",
 		title: "august 28, especial dinner with francesca lombardo",
 		date: "September 10th | 2 - 10 PM",
 		image: september_10,
 	},
 	{
-		id: 3,
+		id: 4,
 		url: "https://www.ticketfairy.com/event/marino-canal-12sep2021/",
 		title: "august 28, especial dinner with francesca lombardo",
 		date: "September 12th | 2 - 10 PM",
 		image: september_12,
 	},
 	{
-		id: 4,
+		id: 5,
 		url: "https://www.ticketfairy.com/event/hunter-game-16sep2021/",
 		title: "august 28, especial dinner with francesca lombardo",
 		date: "September 16th | 2 - 10 PM",
 		image: september_16,
 	},
 	{
-		id: 5,
+		id: 6,
 		url: "https://www.ticketfairy.com/event/nicole-moudaber-17sep2021/",
 		title: "august 28, especial dinner with francesca lombardo",
 		date: "September 17th | 2 - 10 PM",
